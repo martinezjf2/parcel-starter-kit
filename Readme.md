ae9d610d5aa5c731b9ee1e355df96c56dbeb0723
Jeffrey Martinez <martinezjf2@gmail.com>
2024-06-13 11:18:16 -0400

    setup parcel starter kit for react

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 run parcel src/index.html to run the server for this application
+
+add --no-minify to the production key within the scripts if you don't want to minify the file.
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
-var numbers = require('./numbers.js')
-var math = require('./math.js')
-console.log(numbers)
-console.log(math.add((numbers.Johny, numbers.James)));
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
+const App = () => {
+    return <h1>This is my react app</h1>
+}
+
+const appDiv = document.getElementById("app")
+const root = createRoot(appDiv)
+root.render(<App />)
