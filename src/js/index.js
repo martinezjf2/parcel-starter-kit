4d4f58072f6c7a036ef9c484769263af4b8e8c9e
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2024-08-16 13:29:18 -0400

    added some notes and new packages for react

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,27 @@ import '../scss/main.scss';
 
 import React from "react";
 import ReactDOM from "react-dom";
+// If you wanted to implement routes, make sure to wrap App component within BrowserRouter example below lines 19
+import { BrowserRouter } from 'react-router-dom';
 import App from "./App.js";
 
 const appDiv = document.getElementById("app");
 ReactDOM.render(<App />, appDiv);
+
+
+// ReactDOM.render(
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>, appDiv);
+
+
+// export default function App() {
+//     return (
+      
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route path="/resume" element={<Resume />} />
+//         </Routes>
+      
+//     );
+// }
